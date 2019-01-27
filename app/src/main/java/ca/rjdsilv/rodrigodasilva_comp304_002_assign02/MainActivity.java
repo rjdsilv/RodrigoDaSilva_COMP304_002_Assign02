734636d08b4a664808843e7970f170b0951258e1
Rodrigo Januario da Silva <rjdsilv@gmail.com>
2019-01-27 16:02:25 -0500

    Created the first screen

---
+++ app/src/main/java/ca/rjdsilv/rodrigodasilva_comp304_002_assign02/MainActivity.java
@@ -0,0 +1,13 @@
+package ca.rjdsilv.rodrigodasilva_comp304_002_assign02;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
