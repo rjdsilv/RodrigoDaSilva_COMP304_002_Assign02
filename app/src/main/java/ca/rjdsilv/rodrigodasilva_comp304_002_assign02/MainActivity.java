47b45bc2097ec46852c050d6e4ac77536c6daa02
Rodrigo Januario da Silva <rjdsilv@gmail.com>
2019-01-27 16:47:14 -0500

    Calling the second screen.

--- app/src/main/java/ca/rjdsilv/rodrigodasilva_comp304_002_assign02/MainActivity.java
+++ app/src/main/java/ca/rjdsilv/rodrigodasilva_comp304_002_assign02/MainActivity.java
@@ -1,13 +1,34 @@
 package ca.rjdsilv.rodrigodasilva_comp304_002_assign02;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
+/**
+ * Main activity representing the application's main screen.
+ *
+ * @author Rodrigo da Silva
+ * @version 1.0.0
+ */
 public class MainActivity extends AppCompatActivity {
-
+    /**
+     * Creates the activity.
+     *
+     * @param savedInstanceState The saved instance to be used.
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    /**
+     * Event handler for the 'Buy Your Ticket' button click.
+     *
+     * @param view The view to be used.
+     */
+    public void onBuyTicketsClick(View view) {
+        startActivity(new Intent(this, MovieListActivity.class));
     }
 }
---
+++ app/src/main/java/ca/rjdsilv/rodrigodasilva_comp304_002_assign02/MovieListActivity.java
@@ -0,0 +1,13 @@
+package ca.rjdsilv.rodrigodasilva_comp304_002_assign02;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class MovieListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_movie_list);
+    }
+}
